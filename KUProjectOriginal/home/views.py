ef01c9448e9c95ed7eadb0e117c788ff1a33f07a
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2022-07-04 18:54:38 +0530

    Update views.py

--- KUProjectOriginal/home/views.py
+++ KUProjectOriginal/home/views.py
@@ -159,11 +159,11 @@ def validate(request):
         request.session['ptime'] = ptime = request.POST.get('ptime')
         request.session['otf'] = onTimef = 0
         request.session['onTime'] = onTime = request.POST.get('onTime')
-
+        
        if(ptime == 'false' and onTime == 'true'):
             onTimef = request.FILES['onTimef']
             file = fs.save(onTimef.name, onTimef)
-        request.session['otf'] = file
+            request.session['otf'] = file
         prephd = request.FILES['prephd']
         file = fs.save(prephd.name, prephd)
         request.session['prephd'] = file
