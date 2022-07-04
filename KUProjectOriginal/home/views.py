07f674b468c249cc99091dfd785da313f1e913fc
Nithin S <77008788+Nithin-Tinu@users.noreply.github.com>
2022-07-04 17:49:18 +0530

    Update views.py

--- KUProjectOriginal/home/views.py
+++ KUProjectOriginal/home/views.py
@@ -162,8 +162,8 @@ def validate(request):
 
         if(ptime == 'false' and onTime == 'true'):
             onTimef = request.FILES['onTimef']
-        file = fs.save(onTimef.name, onTimef)
-        request.session['otf'] = file
+            file = fs.save(onTimef.name, onTimef)
+            request.session['otf'] = file
         prephd = request.FILES['prephd']
         file = fs.save(prephd.name, prephd)
         request.session['prephd'] = file
